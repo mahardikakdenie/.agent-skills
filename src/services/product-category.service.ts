42817f69b676d1ecf4d0db94566105a165f39731
Sumans <sumans@deptechdigital.com>
2024-10-02 10:03:54 +0700

    product category

--- src/services/product-category.service.ts
+++ src/services/product-category.service.ts
@@ -11,7 +11,7 @@ export interface ProductCategories {
   claim_config: string;
 }
 
-export class ProductCategoryService {
+export class ProductCategoriesService {
   private httpClient: IHttpClient;
   then: any;
 
@@ -25,7 +25,8 @@ export class ProductCategoryService {
       },
     });
   }
-  async getCategory(): Promise<any> {
+
+  async getCategories(): Promise<any> {
     try {
       const response = await this.httpClient.get("/v1/categories");
       return (response as { data: ProductCategories[] }).data;
@@ -35,9 +36,27 @@ export class ProductCategoryService {
     }
   }
 
-  async deleteCategory(id: string): Promise<any> {
+  async deleteCategories(id: string): Promise<any> {
     try {
       return await this.httpClient.delete("/v1/categories/" + id);
+    } catch (error) {
+      console.error("Request failed:", error);
+      throw error;
+    }
+  }
+
+  async saveCategories(data: any): Promise<any> {
+    try {
+      return await this.httpClient.post("/v1/categories/", data);
+    } catch (error) {
+      console.error("Request failed:", error);
+      throw error;
+    }
+  }
+
+  async updateCategories(data: any, id: string): Promise<any> {
+    try {
+      return await this.httpClient.put("/v1/categories/" + id, data);
     } catch (error) {
       console.error("Request failed:", error);
       throw error;
