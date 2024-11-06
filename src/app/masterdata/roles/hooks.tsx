91f913eef2ce9c984e8c6a39e94f08725d7d6572
Sumans <sumans@deptechdigital.com>
2024-11-06 15:56:52 +0700

    add role list

--- src/app/masterdata/roles/hooks.tsx
+++ src/app/masterdata/roles/hooks.tsx
@@ -1,55 +1,45 @@
-import { User, UserService } from "@/services/masterdata/user.service";
-import { channel } from "process";
+import { RoleResponse, RoleService } from "@/services/masterdata/roles.service";
 import { useState } from "react";
 
-export const useUser = () => {
-  const userService = new UserService();
+export const useRole = () => {
+  const roleService = new RoleService();
 
-  const [users, setUsers] = useState<User[]>([]);
-  const [user, setUser] = useState<any[]>([]);
-  const [channels, setChannels] = useState<any[]>([]);
+  const [roles, setRoles] = useState<RoleResponse[]>([]);
+  const [role, setRole] = useState<any[]>([]);
 
-  const fetchUser = async (search: any) => {
-    const { data } = await userService.getUser(search);
-    setUsers(data);
+  const fetchRole = async (search: any) => {
+    const { data } = await roleService.getRole(search);
+    setRoles(data);
   };
 
-  const fetchChannels = async (search: any) => {
-    const { data } = await userService.getChannel(search);
-    setChannels(data);
-  };
-
-  const fetchUserById = async (id: string) => {
-    const response = await userService.getUserById(id);
+  const fetchRoleById = async (id: string) => {
+    const response = await roleService.getRoleById(id);
     return response;
   };
 
-  const saveUser = async (data: any) => {
-    const { data: response } = await userService.saveUser(data);
+  const addRole = async (data: any, id: string) => {
+    const { data: response } = await roleService.addRole(data, id);
     return response;
   };
 
-  const updateUser = async (data: any, id: string) => {
-    const { data: response } = await userService.updateUser(data, id);
+  const updateRole = async (data: any, id: string) => {
+    const { data: response } = await roleService.updateRole(data, id);
     return response;
   };
 
-  const deleteUser = async (id: string) => {
-    const { data: response } = await userService.deleteUser(id);
+  const deleteRole = async (id: string) => {
+    const { data: response } = await roleService.deleteRole(id);
     return response;
   };
 
   return {
-    user,
-    saveUser,
-    updateUser,
-    deleteUser,
-    fetchUser,
-    fetchUserById,
-    users,
-    setUsers,
-    channel,
-    channels,
-    fetchChannels,
+    role,
+    addRole,
+    updateRole,
+    deleteRole,
+    fetchRole,
+    fetchRoleById,
+    roles,
+    setRoles,
   };
 };
---
+++ src/services/masterdata/roles.service.ts
@@ -0,0 +1,70 @@
+import { AxiosHttpClient } from "@/lib/axios-http-client";
+import { IHttpClient } from "../../lib/http-client-interface";
+import Cookies from "universal-cookie";
+import qs from "qs";
+
+export interface RoleResponse {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string;
+  name: string;
+  description: string;
+  data: any;
+  meta: any;
+}
+
+export class RoleService {
+  private authHttpClient: IHttpClient;
+
+  constructor() {
+    this.authHttpClient = new AxiosHttpClient({
+      baseURL: process.env.NEXT_PUBLIC_AUTH_SERVICE_URL,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + process.env.NEXT_PUBLIC_AUTH_TOKEN,
+      },
+    });
+  }
+
+  async getRole(page?: number, rowsPerPage?: number): Promise<RoleResponse> {
+    const params: any = {
+      page: page,
+      pageSize: rowsPerPage,
+    };
+
+    const queryString = qs.stringify(params, { arrayFormat: "brackets" });
+    return this.authHttpClient.get(`/role/?${queryString}`);
+  }
+
+  async getRoleById(id: string): Promise<any> {
+    return this.authHttpClient.get("/role/" + id);
+  }
+
+  async deleteRole(id: string): Promise<any> {
+    try {
+      return await this.authHttpClient.delete("/role/" + id);
+    } catch (error) {
+      console.error("Request failed:", error);
+      throw error;
+    }
+  }
+
+  async addRole(data: any, id: string): Promise<any> {
+    try {
+      return await this.authHttpClient.post("role/", data);
+    } catch (error) {
+      console.error("Request failed:", error);
+      throw error;
+    }
+  }
+
+  async updateRole(data: any, id: string): Promise<any> {
+    try {
+      return await this.authHttpClient.put("/role/" + id, data);
+    } catch (error) {
+      console.error("Request failed:", error);
+      throw error;
+    }
+  }
+}
