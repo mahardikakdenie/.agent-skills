739a2ecb071416768faa47f7dcbfc9c9264bb7fd
Hamizan Shafiq <hamizan@bizniaga-tech.com>
2024-11-13 16:04:52 +0800

    include search query on sanction

--- src/services/sanction.service.ts
+++ src/services/sanction.service.ts
@@ -47,6 +47,13 @@ export class SanctionService {
 
   }
 
+  async getSanctionSearchQuery(query: string, page: number, limit: number): Promise<Response> {
+    if (page <= 0) {
+      page = 1;
+    }
+    return this.httpClientSanction.get(`api/v1/blacklist/search/query?query=${query}&page=${page}&limit=${limit}`);
+  }
+
   async getSanctionList(page: number, limit: number): Promise<Response> {
     if (page <= 0) {
       page = 1;
