bd57622477e16b45331276127ce3788ddd5d1b3f
Sumans <sumans@deptechdigital.com>
2024-12-02 11:07:41 +0700

    implement page management

---
+++ src/app/masterdata/page-management/hooks.tsx
@@ -0,0 +1,48 @@
+import {
+  PagesResponse,
+  PagesService,
+} from "@/services/masterdata/page.service";
+import { useState } from "react";
+
+export const usePages = () => {
+  const pagesService = new PagesService();
+
+  const [productsPages, setProductsPages] = useState<PagesResponse[]>([]);
+  const [categories, setPages] = useState<any[]>([]);
+
+  const fetchPages = async (search: any) => {
+    const { data } = await pagesService.getPages();
+    setPages(data);
+  };
+
+  const fetchPagesById = async (id: string) => {
+    const response = await pagesService.getPagesById(id);
+    return response;
+  };
+
+  const savePages = async (data: any, id: string) => {
+    const { data: response } = await pagesService.savePages(data, id);
+    return response;
+  };
+
+  const updatePages = async (data: any, id: string) => {
+    const { data: response } = await pagesService.updatePages(data, id);
+    return response;
+  };
+
+  const deletePages = async (id: string) => {
+    const { data: response } = await pagesService.deletePages(id);
+    return response;
+  };
+
+  return {
+    categories,
+    savePages,
+    updatePages,
+    deletePages,
+    fetchPages,
+    fetchPagesById,
+    productsPages,
+    setProductsPages,
+  };
+};
