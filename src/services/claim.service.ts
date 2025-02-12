beebe5fad115dc86bb81c6a4af85a4a3d0da6baf
Shelly Sinaga <shelly.sinaga@gmail.com>
2025-02-12 11:13:35 +0700

    feat: init interface based on BE response for claim history page

--- src/services/claim.service.ts
+++ src/services/claim.service.ts
@@ -113,6 +113,25 @@ export interface ClaimChannel {
   form: string;
 }
 
+export interface ClaimHistoryDetail {
+  claimId: string;
+  insuredName: string;
+  status: string;
+  currency: string;
+  paymentType: string;
+  submittedDate: string;
+  claimAmount: number;
+  paid: number;
+  remainingLimit: number;
+}
+
+export interface ClaimHistorySummary {
+  data: ClaimHistoryDetail[];
+  plans: { planId: string; planName: string }[];
+  totalLimit: number;
+  totalPaid: number;
+  remainingClaimLimit: number;
+}
 export class ClaimService {
   private httpClient: IHttpClient;
 
@@ -206,24 +225,21 @@ export class ClaimService {
     return this.httpClient.get(`/v1/claim-histories?claim=${id}`);
   }
 
-  async getClaimsHistoriesList(
-    page: number,
-    rowsPerPage: number,
-    searchData?: string,
-  ): Promise<ClaimResponse> {
-    const params: any = {
-      page: page,
-      limit: rowsPerPage,
-    };
-
-    if (searchData) {
-      params["keyword"] = searchData;
-    }
-
+  async getClaimsHistoriesList({
+    searchData,
+    planId,
+  }: {
+    searchData: string;
+    planId?: string;
+  }): Promise<{ data: ClaimHistorySummary[] }> {
+    const params: any = {};
+  
+    if (planId) params["plan_id"] = planId;
+    if (searchData) params["search"] = searchData;
+  
     const queryString = qs.stringify(params, { arrayFormat: "brackets" });
-    return this.httpClient.get(`/v1/claims-histories?${queryString}`);
+    return this.httpClient.get(`/v1/claims/claim-list-limit?${queryString}`);
   }
-
   async updateClaimStatus(
     id: string,
     data: any,
