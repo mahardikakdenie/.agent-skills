8214a33d7099f27f4aa975e5ffdf96b778faacb7
Shelly Sinaga <shelly.sinaga@gmail.com>
2025-02-17 23:06:13 +0700

    Revert "feat: add loading animation on claim history page"
    This reverts commit 42046b1b24fb5bf95c762a37ea638c9778b9d4ba.

--- src/app/claim-list/import/page.tsx
+++ src/app/claim-list/import/page.tsx
@@ -44,9 +44,9 @@ const ImportPage = () => {
 
   const handleFileSelection = async (file: File) => {
     if (
-      file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
-      file.type === "application/vnd.ms-excel" ||
-      file.type === "text/csv"
+      file.type ===
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+      file.type === "application/vnd.ms-excel"
     ) {
       setSelectedFile(file);
       try {
