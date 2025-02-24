9522f88f1f81b953a049a43ada98d9707be1c2e0
Shelly Sinaga <shelly.sinaga@gmail.com>
2025-02-24 11:55:42 +0700

    feat: add import data guide function

--- src/services/claim.service.ts
+++ src/services/claim.service.ts
@@ -317,11 +317,18 @@ export class ClaimService {
     category: string;
   }) {
     const { data, input, channel, category } = importData;
-    return this.httpClient.post(`/v1/claims/import`, {
+    return await this.httpClient.post(`/v1/claims/import`, {
       data,
       input: "Data",
       channel,
       category,
     });
   }
+
+  async importDataGuide(params: { channel: string; category: string }): Promise<{ data: { data: any }[] }> {
+    const { channel, category } = params;
+    const queryString = qs.stringify({ channel, category }, { arrayFormat: "brackets" });
+  
+    return await this.httpClient.get(`/v1/claims/import-data-guide?${queryString}`);
+  }  
 }
