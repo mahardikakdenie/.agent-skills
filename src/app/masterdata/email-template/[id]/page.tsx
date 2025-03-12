b1b3b9ce621d8ddc01696547e5d534770addb8e7
Sumans <sumans@deptechdigital.com>
2025-03-12 16:05:58 +0700

    fixed bug

--- src/app/masterdata/email-template/[id]/page.tsx
+++ src/app/masterdata/email-template/[id]/page.tsx
@@ -84,7 +84,7 @@ const EditPage = ({ params }: { params: { id: string } }) => {
     formState: { errors },
   } = useForm({
     defaultValues: {
-      category: selectedCategoryId,
+      category: "",
       insurance: selectedInsuranceId,
       product: selectedProductId,
       plan: selectedPlanId,
@@ -335,7 +335,7 @@ const EditPage = ({ params }: { params: { id: string } }) => {
                       <SelectGroup>
                         {categories.map((categorie: any) => (
                           <SelectItem key={categorie.id} value={categorie.id}>
-                            {categorie.name
+                            {categorie?.name
                               .split("-")
                               .map(
                                 (word: string) =>
@@ -383,13 +383,7 @@ const EditPage = ({ params }: { params: { id: string } }) => {
                       <SelectGroup>
                         {insurances.map((insurance: any) => (
                           <SelectItem key={insurance.id} value={insurance.id}>
-                            {insurance.name
-                              .split("-")
-                              .map(
-                                (word: string) =>
-                                  word.charAt(0).toUpperCase() + word.slice(1)
-                              )
-                              .join(" ")}
+                            {insurance.name}
                           </SelectItem>
                         ))}
                       </SelectGroup>
@@ -527,30 +521,27 @@ const EditPage = ({ params }: { params: { id: string } }) => {
                 control={control}
                 rules={{ required: "Journey is required" }}
                 render={({ field }) => (
-                  console.log(selectedJourneyId),
-                  (
-                    <Select
-                      key={selectedJourneyId}
-                      value={selectedJourneyId ?? ""}
-                      onValueChange={(value) => {
-                        field.onChange(value);
-                        setSelectedJourneyId(value);
-                      }}
-                    >
-                      <SelectTrigger className="w-full h-12 border-gray-300 select-status bg-transparent hover:cursor-pointer py-2">
-                        <SelectValue placeholder="Select Journey" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          {journey.map((jour: any) => (
-                            <SelectItem key={jour.id} value={jour.code}>
-                              {jour.name}
-                            </SelectItem>
-                          ))}
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                  )
+                  <Select
+                    key={selectedJourneyId}
+                    value={selectedJourneyId ?? ""}
+                    onValueChange={(value) => {
+                      field.onChange(value);
+                      setSelectedJourneyId(value);
+                    }}
+                  >
+                    <SelectTrigger className="w-full h-12 border-gray-300 select-status bg-transparent hover:cursor-pointer py-2">
+                      <SelectValue placeholder="Select Journey" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        {journey.map((jour: any) => (
+                          <SelectItem key={jour.id} value={jour.code}>
+                            {jour.name}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
                 )}
               />
               {errors.journey && (
