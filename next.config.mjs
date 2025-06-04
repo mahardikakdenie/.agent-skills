13b3f4ca73f40f1b95f9b72567de6128b60ee5f3
iksanmaricar-fti <iksan.maricar@friendsure.id>
2025-06-04 15:36:37 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['friendsure-spaces.sgp1.digitaloceanspaces.com'],
+      domains: [
+          'friendsure-spaces.sgp1.digitaloceanspaces.com',
+          'https://cdn.frndsr.tech',
+          'https://cdn-stg.frndsr.tech',
+          'https://cdn.friendsure.tech',
+          'https://cdn-stg.friendsure.tech',
+      ],
     },
   };
   
