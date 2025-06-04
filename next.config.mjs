775fca0408274da297f15de79cd5b0d12dbc7841
Surya Djayadi <surya.djayadi@friendsure.id>
2025-06-04 15:55:31 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,10 @@ const nextConfig = {
     images: {
       domains: [
           'friendsure-spaces.sgp1.digitaloceanspaces.com',
-          'https://cdn.frndsr.tech',
-          'https://cdn-stg.frndsr.tech',
-          'https://cdn.friendsure.tech',
-          'https://cdn-stg.friendsure.tech',
+          'cdn.frndsr.tech',
+          'cdn-stg.frndsr.tech',
+          'cdn.friendsure.tech',
+          'cdn-stg.friendsure.tech',
       ],
     },
   };
