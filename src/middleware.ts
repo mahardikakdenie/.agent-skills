483bfa40ed43f3990bc48a44718807675f5b3bcd
Shelly Sinaga <shelly.sinaga@gmail.com>
2025-06-23 13:17:20 +0700

    feat: add login page inside middleware logic to handle logged-in user accessing the login page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,27 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+import { LOGIN, DASHBOARD_TRANSACTION } from "@/constants/routes";
+
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
+  const { pathname } = request.nextUrl;
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/", request.url));
+  const isProtectedRoute = pathname.startsWith("/protected");
+  const isLoginPage = pathname === LOGIN;
+
+  if (isProtectedRoute && !token) {
+    return NextResponse.redirect(new URL(LOGIN, request.url));
+  }
+
+  if (token && isLoginPage) {
+    return NextResponse.redirect(new URL(DASHBOARD_TRANSACTION, request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/protected/:path*"],
-};
+  matcher: ["/protected/:path*", "/"],
+}
+
