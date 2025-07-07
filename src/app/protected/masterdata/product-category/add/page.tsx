952ab873da1e684b3bc7f2d12d9e92c1974bbe0e
Rifqi Fadhil <rifqife@gmail.com>
2025-07-07 15:29:32 +0700

    Addition: Adding icon column to create/update forms and category list table | Refactor: create/update form become a single reusable component for both pages

--- src/app/protected/masterdata/product-category/add/page.tsx
+++ src/app/protected/masterdata/product-category/add/page.tsx
@@ -1,40 +1,19 @@
 "use client";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Input } from "@/components/ui/input";
+
 import WithSidebar from "@/hoc/with-sidebar";
-import { usePathname, useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
-import { Check, ChevronLeft } from "react-feather";
-import { Controller, useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
-import { useCategories } from "../hooks";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { hasPermission } from "@/context/auth.context";
 import { FORBIDDEN } from "@/constants/routes";
+import ProductCategoryForm from "@/components/forms/product-category.form";
 
-const AddProductCategory = ({ params }: { params: { id: string } }) => {
+const AddProductCategory = () => {
   const router = useRouter();
-  const { id } = params;
-  const [saveSuccess, setSaveSuccess] = useState<boolean | null>(null);
-  const path = usePathname();
-
-  const [name, setName] = useState("");
-
-  const { fetchCategories, saveCategories } = useCategories();
-
-
-  const [hasAccess, setHasAccess] = useState<boolean | null>(null);
 
   useEffect(() => {
     const checkAccess = async () => {
       const access = await hasPermission("Masterdata.Create");
-      setHasAccess(access);
+
       if (!access) {
         router.push(FORBIDDEN);
       }
@@ -43,132 +22,7 @@ const AddProductCategory = ({ params }: { params: { id: string } }) => {
     checkAccess();
   }, [router]);
 
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm({
-    shouldUnregister: false,
-    defaultValues: {
-      name,
-    },
-    values: {
-      name,
-    },
-  });
-
-  const onSubmit = async (data: any) => {
-    try {
-      await saveCategories(data);
-      setSaveSuccess(true);
-    } catch (error) {
-      setSaveSuccess(false);
-    }
-  };
-
-  useEffect(() => {
-    if (id) {
-      (async () => {
-        await fetchCategories({});
-      })();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id]);
-
-  useEffect(() => {
-    if (saveSuccess === true) {
-      alert("Data berhasil disimpan!");
-      router.back();
-    } else if (saveSuccess === false) {
-      alert("Terjadi kesalahan saat menyimpan data.");
-    }
-    setSaveSuccess(null);
-  }, [saveSuccess, router]);
-
-  return (
-    <div className="flex flex-col w-full">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="bg-white md:px-6 p-4 flex items-center">
-          <div>
-            <Breadcrumb className="sm:block hidden">
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbLink>Masterdata</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbLink
-                    className="cursor-pointer"
-                    onClick={() => router.back()}
-                  >
-                    Product Category
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Add</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-            <h2 className="text-black font-bold sm:text-2xl text-lg sm:mt-2">
-              Add Product Category
-            </h2>
-          </div>
-
-          <div className="flex ml-auto">
-            <div
-              onClick={() => router.back()}
-              className="font-semibold ml-auto items-center flex gap-1 text-red-700 text-sm cursor-pointer"
-            >
-              <ChevronLeft className="w-4 h-4" />
-              Back
-            </div>
-            <Button
-              type="submit"
-              className="bg-[#F5BA41] text-black hover:bg-[#e6a92d] ml-5 rounded-full px-5"
-            >
-              <Check className="mr-2 w-4 h-4" />
-              Save
-            </Button>
-          </div>
-        </div>
-        <div className="flex flex-col w-full p-4 md:p-6 gap-4">
-          <div className="p-4 sm:p-6 bg-white rounded-lg flex flex-col gap-4">
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-700 mb-2"
-              >
-                Category Name
-              </label>
-              <Controller
-                name="name"
-                control={control}
-                defaultValue=""
-                rules={{ required: "Category Name is required" }}
-                render={({ field }) => (
-                  <Input
-                    type="text"
-                    id="name"
-                    placeholder="Insert Category Name"
-                    {...field}
-                    className={`mt-1 block w-full h-12 ${
-                      errors.name ? "border-red-500" : "border-gray-300"
-                    } rounded-md shadow-sm`}
-                  />
-                )}
-              />
-              {errors.name && (
-                <p className="text-red-500 text-xs mt-1">
-                  {errors.name.message}
-                </p>
-              )}
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+  return <ProductCategoryForm method="create" />;
 };
 
 const AddProductCategoryWithSidebar = (params: any) =>
