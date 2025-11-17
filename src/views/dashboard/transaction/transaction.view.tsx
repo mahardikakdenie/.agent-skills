b70e182e46ee4b6c647280e2a3751000e88b6c98
nauvaldeptech <nauval@deptechdigital.id>
2025-11-17 13:12:58 +0700

    fix(dashboard): set default date values to today date in dashboard

--- src/views/dashboard/transaction/transaction.view.tsx
+++ src/views/dashboard/transaction/transaction.view.tsx
@@ -32,8 +32,8 @@ export const DashboardTransaction = () => {
   const [insuranceOptions, setInsuranceOptions] = useState<any[]>([]);
   const [selectedPlan, setSelectedPlan] = useState<string>("");
   const [planOptions, setPlanOptions] = useState<any[]>([]);
-  const [from, setFrom] = useState("");
-  const [to, setTo] = useState("");
+  const [from, setFrom] = useState(format(new Date(), 'yyyy-MM-dd'));
+  const [to, setTo] = useState(format(new Date(), 'yyyy-MM-dd'));
 
   const handleDateChange = (startDate: string, endDate: string) => {
     setFrom(startDate);
